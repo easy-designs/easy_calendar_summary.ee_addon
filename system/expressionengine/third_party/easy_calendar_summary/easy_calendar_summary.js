3f425b3249c5c330967435478bb46d5d6dbc5537
aarongustafson <aaron@easy-designs.net>
2013-09-11 17:25:41 -0400

    Added some functionality to let you jump to an page in the picker that displays the selected event

--- system/expressionengine/third_party/easy_calendar_summary/easy_calendar_summary.js
+++ system/expressionengine/third_party/easy_calendar_summary/easy_calendar_summary.js
@@ -12,7 +12,8 @@
 	if ( $calendar.length > 0 )
 	{
 		// templates
-		$dates = $('<div class="easy-calendar-summary-dates"><strong>{dates_title}</strong><ol/></div>');
+		$dates = $('<div class="easy-calendar-summary-dates"><strong>{dates_title}</strong><ol/></div>')
+					.on( tap_evt, 'li', picker_change );
 		$date = $('<li/>');
 		
 		$('<style media="screen"/>')
@@ -49,8 +50,8 @@
 			{
 				if ( $these_dates.length === 0 )
 				{
-					$dates.clone()
-						.appendTo( $rule );
+					$these_dates = $dates.clone( true )
+										.appendTo( $rule );
 				}
 				
 				// watch!
@@ -80,90 +81,23 @@
 				
 			});
 		}
+		
+		// switch the calendar view when a user clicks a date
+		function picker_change()
+		{
+			var $date = $(this),
+				date = $date.text().split('-');
+			
+			$date.closest('.rule')
+				.find('.ui-datepicker-year')
+					.val( parseInt( date[0], 10 ) )
+					.trigger('change')
+					.end()
+				.find('.ui-datepicker-month')
+					.val( parseInt( date[1], 10 ) - 1 )
+					.trigger('change');
+		}
 
 	}
 	
 })(this,jQuery);
-
-//;(function(window,$){
-//
-//	var	tap_evt = 'click',
-//	 	$calendar = $('#calendar_fields'),
-//		$dates, $date;
-//	
-//	if ( 'touchend' in window )
-//	{
-//		tap_evt = 'touchend';
-//	}
-//
-//	if ( $calendar.length > 0 )
-//	{
-//		// templates
-//		$dates = $('<div class="easy-calendar-summary-dates"><strong>{dates_title}</strong><ol/></div>');
-//		$date = $('<li/>');
-//		
-//		$('<style media="screen"/>')
-//			.text('{styles}')
-//			.appendTo('head');
-//		
-//		function update_dates()
-//		{
-//			console.log('update');
-//			var $rule = $(this),
-//				dates = $rule.find('.picker_three').data('values'),
-//				$these_dates = $rule.find('.easy-calendar-summary-dates'),
-//				interval = $rule.find('[name=interval]').val(),
-//				$date_list;
-//			
-//			if ( interval != 'select_dates' )
-//			{
-//				$these_dates.hide();
-//			}
-//			else
-//			{
-//				if ( $these_dates.length === 0 )
-//				{
-//					$these_dates = $dates.clone()
-//										.appendTo( $rule );
-//				}
-//				
-//				// empty the list
-//				$date_list = $these_dates.find('ol').empty();
-//				
-//				// sort the dates numerically
-//				dates.sort();
-//				
-//				// fill it back up
-//				$(dates).each(function(){
-//					
-//					$date.clone()
-//						.text( this )
-//						.appendTo( $date_list );
-//					
-//				});
-//			}
-//		}
-//		
-//		function add_handlers()
-//		{
-//			console.log('hi');
-//			$calendar.find('.rule')
-//				// remove existing
-//				//.off( tap_evt + ' change', update_dates )
-//				// add new
-//				.on( tap_evt + ' change', update_dates );
-//		}
-//
-//		// handle new rules
-//		$('#calendar_calendars button').on( tap_evt, add_handlers );
-//		
-//		// once the page is done loading…
-//		$(window).on('load',function(){
-//			add_handlers();
-//			$calendar.find('.rule')
-//				.triggerHandler( tap_evt );
-//		});
-//		
-//	}
-//	
-//})(this,jQuery);
